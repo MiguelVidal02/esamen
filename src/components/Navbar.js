94c32dd183ca06ae463ae069e82a70fef83859ab
MIGUELAGV02 <al222210644@gmail.com>
2025-04-26 11:42:25 -0600

    Descripcionde prod

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav style={{ padding: 10 }}>
       <Link to="/users" style={{ margin: 10 }}>Usuarios</Link>
-      <Link to="/products" style={{ margin: 10 }}>Productos</Link>
+      <Link to="/products" style={{ margin: 10 }}>Productos TODOS :V</Link>
     </nav>
   );
 }
